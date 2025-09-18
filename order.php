a9e0ac0f106f189317d4e5e42fd0533afc9b74cb
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2025-09-18 20:17:14 +0200

    Update order.php

--- order.php
+++ order.php
@@ -1,5 +1,9 @@
 <?php
 echo "A neved: " . $_POST["nev"] . "<br>";
 echo "Ezt rendelted: " . $_POST["txt"] . "<br>";
+if ($_COOKIE['location'] == "") {
+    echo "Nincs megadva helyadat!";
+} else {
+    echo '<a href="https://www.google.com/maps/place/" . $_COOKIE["location"];>Ide</a>';
+}
 ?>
-<a href="https://www.google.com/maps/place/<?= $_COOKIE['location']; ?>">Ide</a>
